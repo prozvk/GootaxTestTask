13549724ef3a8cd036a64a6674244fb06270a985
Vladislav Prozorov <prozvk@outlook.com>
2022-10-03 20:09:59 +0400

    code review

---
+++ GootaxTestTask/Extensions/CGFloat + Extension.swift
@@ -0,0 +1,8 @@
+//
+//  CGFloat + Extension.swift
+//  GootaxTestTask
+//
+//  Created by MacPro on 03.10.2022.
+//
+
+import Foundation
---
+++ GootaxTestTask/Extensions/CGPoint + Extension.swift
@@ -0,0 +1,8 @@
+//
+//  CGPoint + Extension.swift
+//  GootaxTestTask
+//
+//  Created by MacPro on 03.10.2022.
+//
+
+import Foundation
